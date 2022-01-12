72df7a58ac1a7e57a3d4c9ab78cebb2ed6a9e282
janvorac <vorac@mail.muni.cz>
2022-01-12 08:11:12 +0100

    created guess/index.html and defined models

--- training_site/guess/models.py
+++ training_site/guess/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Game(models.Model):
+    date = models.DateField('played on')
+
+    def __str__(self):
+        return f'played on {self.date}'
+
+
+class Guessed(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    number = models.IntegerField()
+
+    def __str__(self):
+        return self.number
--- training_site/guess/views.py
+++ training_site/guess/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.views import generic
+from .models import Game, Guessed
 
 # Create your views here.
 
 
-def index(request):
-    return render(request, 'guess/index.html', {})
+class IndexView(generic.ListView):
+    template_name = 'guess/index.html'
+    context_object_name = 'games'
+
+    def get_queryset(self):
+        return Game.objects.filter(date__lte=timezone.now()).order_by('-date')[:5]
